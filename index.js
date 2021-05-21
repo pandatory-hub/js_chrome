4e0a21fb284e320bfaf13f74bb6986db2abde8f2
pandatory <20171238@vision.hoseo.edu>
2021-05-21 22:45:03 +0900

    #2.3

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-const calculator = {
-  plus: function(a, b) {
-    return a + b;
-  },
-
-  minus: function(a, b) {
-    return a - b;
-  },
-
-  multi: function(a, b) {
-    return a * b;
-  },
-
-  divi: function(a, b) {
-    return a / b;
-  }, 
-
-  remain: function(a, b) {
-    return a % b;
-  }
-}
-
-const pplus = calculator.plus(5, 5);
-const mminus = calculator.minus(5, 2);
-const mmulti = calculator.multi(5, 2);
-const ddivi = calculator.divi(6, 2);
-const rremain = calculator.remain(5, 2);
-
-console.log(pplus);
-console.log(mminus);
-console.log(mmulti);
-console.log(ddivi);
-console.log(rremain);
+const title = document.querySelector("#title");
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+document.title = "HelloHello";
