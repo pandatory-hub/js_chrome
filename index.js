9619ebbac2e0963072e1f1f6155c56e34c611cf5
pandatory <20171238@vision.hoseo.edu>
2021-05-21 23:36:27 +0900

    #2.6

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const title = document.querySelector("#title");
 
+const BASE_COLOR = "white";
+const OTHER_COLOR = "#FC427B";
+
 function handleClick() {
-  title.style.color = "blue";
+  const currentColor = title.style.color;
+  if(currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
+
 }
 
+function init() {
+  title.style.color = BASE_COLOR;
+}
 title.addEventListener("click", handleClick);
+
+init();
+
