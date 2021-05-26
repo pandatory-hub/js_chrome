1861e5f79a4869fcdd396bf32042886e0baf3645
pandatory <20171238@vision.hoseo.edu>
2021-05-27 00:09:06 +0900

    #3.10

--- weather.js
+++ weather.js
@@ -1,40 +1,60 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = "917cc6d05e695c78a211ef1e666dc794";
 const COORDS = 'coords';
 
+function getWeather(lat, lng) {
+    fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ) 
+    
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+          const temperature = json.main.temp;
+          const place = json.name;
+          weather.innerText = `${temperature} @ ${place}`;
+        });
+}
+
 function saveCoords(coordsObj) {
-  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function handleGeoSuccess(position) {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  const coordsObj = {
-    latitude,
-    longitude
-  };
-  saveCoords(coordsObj);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
-  console.log('Cant access geo location');
+    console.log('Cant access geo location');
 }
 
 function askForCoords() {
-  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+    navigator
+        .geolocation
+        .getCurrentPosition(handleGeoSuccess, handleGeoError);
 }
 
 function loadCoords() {
-  const loadedCords = localStorage.getItem(COORDS);
-  if(loadedCords === null) {
-    askForCoords();
-  } else {
-    //getWeather
-  }
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
+        askForCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
 
 }
 
 function init() {
-  loadCoords();
+    loadCoords();
 }
 
 init();
